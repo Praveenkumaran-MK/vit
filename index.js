76defa44453a9ef564144e0accc40f7cd75d860d
PremnathSundarrajan <premsundarrajan4@gmail.com>
2025-12-02 17:10:12 +0530

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+// app.post("/extend", asyn(req,res)=>{
+//     const body = await 
+// })
+
+app.listen(3000, ()=>{
+    console.log("running ..")
+})
