b0084a463a8727a972c7ee90f9977a88f7f4fda9
PremnathSundarrajan <premsundarrajan4@gmail.com>
2025-12-02 18:28:51 +0530

    Backend Folder created

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const { Prisma } = require("@prisma/client");
 dotenv.config();
 
 
-// app.post("/extend", asyn(req,res)=>{
-//     const body = await 
-// })
+app.post("/extend", asyn(req,res)=>{
+    const body = await Prisma.user
+})
 
 app.listen(3000, ()=>{
     console.log("running ..")
